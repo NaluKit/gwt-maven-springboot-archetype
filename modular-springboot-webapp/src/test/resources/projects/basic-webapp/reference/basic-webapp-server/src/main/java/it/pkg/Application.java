a03888ff5885b5295a0f34ada2f5fbbc718b9cfd
Frank Hossfeld <frank.hossfeld@googlemail.com>
2020-11-03 09:03:53 +0100

    improve Spring Boot Application for use in web container

--- modular-springboot-webapp/src/test/resources/projects/basic-webapp/reference/basic-webapp-server/src/main/java/it/pkg/Application.java
+++ modular-springboot-webapp/src/test/resources/projects/basic-webapp/reference/basic-webapp-server/src/main/java/it/pkg/Application.java
@@ -2,15 +2,23 @@ package it.pkg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan
-public class Application {
+public class Application
+    extends SpringBootServletInitializer {
   
   public static void main(String[] args) {
     SpringApplication.run(Application.class,
                           args);
+  }
+  
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(Application.class);
   }
   
 }

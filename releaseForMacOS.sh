e3acaf65e40079beed7641d66bfa0c81b61c2464
Frank Hossfeld <frank.hossfeld@googlemail.com>
2023-12-28 11:14:52 +0100

    update script

--- releaseForMacOS.sh
+++ releaseForMacOS.sh
@@ -2,8 +2,6 @@
 scriptDir="$(cd "$(dirname "$0")" && pwd -P)"
 echo "SricptDir: " ${scriptDir}
 
-export JAVA_HOME=/usr/libexec/java_home -v 1.8.0_392
-
 set -eu
 
 readonly VERSION=$(date +%Y.%-m.%-d)

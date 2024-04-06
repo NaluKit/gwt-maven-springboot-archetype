bea14a9d096cad3341cfa2fa1f8418fdf760cd24
Hua Zhang <hzhang1978@gmail.com>
2024-04-06 11:45:54 +0200

    fix to find a correct serializationPolicyFilePath on a temporary web server started by spring boot application

--- clean-modular-springboot-webapp/src/main/resources/archetype-resources/__rootArtifactId__-server/src/main/java/Application.java
+++ clean-modular-springboot-webapp/src/main/resources/archetype-resources/__rootArtifactId__-server/src/main/java/Application.java
@@ -1,4 +1,4 @@
-package ${package};
+package $
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,19 +6,32 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.io.File;
+import java.util.Objects;
+
 @SpringBootApplication
 @ServletComponentScan
-public class Application
-    extends SpringBootServletInitializer {
-  
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class,
-                          args);
-  }
-  
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-    return builder.sources(Application.class);
-  }
-  
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    @Component
+    public static class EmbeddedServletContainerConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+        @Override
+        public void customize(TomcatServletWebServerFactory factory) {
+            /**
+             * You have to set a document root here, otherwise RemoteServiceServlet will failed to find the
+             * corresponding serializationPolicyFilePath on a temporary web server started by spring boot application:
+             * servlet.getServletContext().getResourceAsStream(serializationPolicyFilePath) returns null.
+             * This has impact that java.io.Serializable can be no more used in RPC, only IsSerializable works.
+             * */
+            factory.setDocumentRoot(new File(Objects.requireNonNull(getClass().getResource("/")).getFile(), "launcherDir"));
+        }
+    }
 }

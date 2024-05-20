94abd2a8030f960c27f13a9561f72608f081a172
Craig Mitchell <mail@craig-mitchell.com>
2024-05-20 12:34:16 +1000

    Fix for running a compiled build, plus allow any web server.

--- modular-springboot-webapp/src/test/resources/projects/basic-webapp/reference/basic-webapp-server/src/main/java/it/pkg/Application.java
+++ modular-springboot-webapp/src/test/resources/projects/basic-webapp/reference/basic-webapp-server/src/main/java/it/pkg/Application.java
@@ -3,27 +3,25 @@ package it.pkg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Objects;
 
 @SpringBootApplication
 @ServletComponentScan
 public class Application
     extends SpringBootServletInitializer {
-  
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class,
                           args);
   }
-  
+
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
     return builder.sources(Application.class);
@@ -31,13 +29,13 @@ public class Application
 
   @Component
   public static class EmbeddedServletContainerConfig
-      implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+      implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
     @Override
-    public void customize(TomcatServletWebServerFactory factory) {
+    public void customize(ConfigurableServletWebServerFactory factory) {
       File laucherDirDirectory = new File(Objects.requireNonNull(getClass().getResource("/"))
                                                  .getFile(),
                                           "launcherDir");
-      if (Files.exists(Path.of(laucherDirDirectory.toURI()))) {
+      if (laucherDirDirectory.exists()) {
         // You have to set a document root here, otherwise RemoteServiceServlet will failed to find the
         // corresponding serializationPolicyFilePath on a temporary web server started by spring boot application:
         // servlet.getServletContext().getResourceAsStream(serializationPolicyFilePath) returns null.
